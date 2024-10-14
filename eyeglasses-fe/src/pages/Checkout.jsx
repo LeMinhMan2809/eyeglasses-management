37c74f044f1b06ac93d8b8357c57dcf3fb95a1da
LeMinhMan2809 <lemann.cth@gmail.com>
2024-10-15 01:21:06 +0700

    Editing UI of checkout page

--- eyeglasses-fe/src/pages/Checkout.jsx
+++ eyeglasses-fe/src/pages/Checkout.jsx
@@ -15,32 +15,81 @@ const Checkout = () => {
   }, []);
 
   return (
-    <div className="ml-[60px] mr-[60px] mt-8">
-      <div className="flex gap-10 w-full">
-        <div className="w-[1000px]">
+    <div className="ml-[100px] mr-[100px] mt-8">
+      <div className="w-full">
+        <div className="w-full">
           <h1 className="text-3xl font-semibold mb-4">Thanh toán</h1>
           <p className="font-semibold text-lg mb-3">Địa chỉ nhận hàng</p>
-          <div class="flex items-center">
+          <div className="flex items-center">
             <input
               type="radio"
               name="address"
-              class="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+              className="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
             />
-            <span class="text-base font-medium ml-3">
+            <span className="text-base font-medium ml-3">
               Nguyễn Văn A, (+84) 901 008 657, Số 100A, Phường Phú Thuận, Quận
               7, Hồ Chí Minh
             </span>
           </div>
-          <div class="flex items-center">
+          <div className="flex items-center">
             <input
               type="radio"
               name="address"
-              class="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
+              className="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500"
             />
-            <span class="text-base font-medium ml-3">
+            <span className="text-base font-medium ml-3">
               Nguyễn Văn A, (+84) 901 008 657, Số 100A, Phường Phú Thuận, Quận
               7, Hồ Chí Minh
             </span>
+          </div>
+
+          <div className="p-5 bg-[#f3f3f3] w-full mt-3 text-lg">
+            <p className="text-center pb-[3rem] font-semibold">Đơn hàng</p>
+
+            <table className="w-full">
+              <thead>
+                <tr>
+                  <th className="w-[20rem] text-left">Sản phẩm</th>
+                  <th>Số lượng</th>
+                  <th>Giá</th>
+                  <th>Thành tiền</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartDataList.map((item, index) => (
+                  <tr
+                    key={index}
+                    className="border-b text-center border-gray-400 mb-5"
+                  >
+                    <td className="flex items-center">
+                      <img
+                        className="w-[100px] mr-5"
+                        src={`${url}/images/${item.product.images}`}
+                        alt=""
+                      />
+                      <p>{item.product.name}</p>
+                    </td>
+                    <td>{item.quantity}</td>
+                    <td>{item.product.price}</td>
+                    <td>{item.product.price * item.quantity}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <div className="flex justify-between mt-5">
+              Phí vận chuyển:
+              <span className="text-lg font-medium">0</span>
+            </div>
+            <div className="flex justify-between mt-5">
+              Tổng cộng:
+              <span className="text-lg font-medium">
+                {cartDataList.reduce(
+                  (total, item) => total + item.product.price * item.quantity,
+                  0
+                )}
+              </span>
+            </div>
           </div>
 
           <div className="mt-5 flex items-center gap-3">
@@ -75,54 +124,12 @@ const Checkout = () => {
               />
               <label htmlFor="bank">Thanh toán qua VNPAY</label>
             </div>
-          </div>
-        </div>
 
-        <div className="p-5 bg-[#f3f3f3] w-[25rem] text-lg">
-          <p className="text-center pb-[3rem] font-semibold">
-            Tóm tắt đơn hàng
-          </p>
-          <div className="flex items-center justify-between text-lg font-medium mb-3">
-            <p>Sản phẩm</p>
-            <p>Thành tiền</p>
-          </div>
-          <hr className="mb-3"></hr>
-          <div>
-            {cartDataList.map((item, index) => (
-              <div className="flex items-center gap-3" key={index}>
-                <img
-                  className="w-[100px]"
-                  src={`${url}/images/${item.product.images}`}
-                  alt=""
-                />
-                <div>{item.product.name}</div>
-                <div>
-                  <span>x</span>
-                  {item.quantity}
-                </div>
-                <div>{item.product.price}</div>
-              </div>
-            ))}
-          </div>
-          <hr className="mt-3"></hr>
-          <div className="flex justify-between mt-5">
-            Phí vận chuyển:
-            <span className="text-lg font-medium">0</span>
-          </div>
-          <div className="flex justify-between mt-5">
-            Tổng cộng:
-            <span className="text-lg font-medium">
-              {cartDataList.reduce(
-                (total, item) => total + item.product.price * item.quantity,
-                0
-              )}
-            </span>
-          </div>
-
-          <div className="flex justify-center mt-5">
-            <button className="text-center px-10 py-2 bg-orange-500 rounded-md">
-              Đặt hàng
-            </button>
+            <div className="flex justify-center mt-5">
+              <button className="text-center px-10 py-2 bg-orange-500 rounded-md">
+                Đặt hàng
+              </button>
+            </div>
           </div>
         </div>
       </div>
