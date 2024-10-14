4a299e08521ed00d355e3b062f7942bd84fe39b0
LeMinhMan2809 <lemann.cth@gmail.com>
2024-10-14 08:55:05 +0700

    Clearing local storage when closed app

--- eyeglasses-fe/src/App.jsx
+++ eyeglasses-fe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -6,6 +6,18 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    const clearCartOnClose = () => {
+      localStorage.removeItem("token");
+      localStorage.removeItem("cart");
+    };
+
+    window.addEventListener("beforeunload", clearCartOnClose);
+
+    return () => {
+      window.removeEventListener("beforeunload", clearCartOnClose);
+    };
+  }, []);
 
   return (
     <>
