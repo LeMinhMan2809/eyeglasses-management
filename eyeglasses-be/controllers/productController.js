3d85d11989bf398ea8574c56a040d83494f06f04
LeMinhMan2809 <lemann.cth@gmail.com>
2024-10-18 16:54:57 +0700

    Modify product controller

--- eyeglasses-be/controllers/productController.js
+++ eyeglasses-be/controllers/productController.js
@@ -6,134 +6,161 @@ const fs = require("fs");
 
 require("dotenv").config();
 
-
 const getProducts = async (req, res) => {
-    try {
-        const products = await productModel.find().populate("category");
-        res.json(products);
-    } catch (error) { 
-        console.log(error);
-        res.json({sucess: false});
-    }
-}
+  try {
+    const products = await productModel.find().populate("category");
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.json({ sucess: false });
+  }
+};
 
 const getProductID = async (req, res) => {
-    try {
-        const products = await productModel.findById(req.params.id).populate();
-        if (!products) return res.status(404).json({message: "Product not found"});
-        res.json(products);
-    } catch (error) {
-        console.log(error);
-        res.json({sucess: false});
-    }
-}
+  try {
+    const products = await productModel.findById(req.params.id).populate();
+    if (!products)
+      return res.status(404).json({ message: "Product not found" });
+    res.json(products);
+  } catch (error) {
+    console.log(error);
+    res.json({ sucess: false });
+  }
+};
 
 const addProducts = async (req, res) => {
-    const existingProduct = await productModel.findOne({name: req.body.name});
-    if (existingProduct) {
-        return res.json({message: "Sản phẩm đã tồn tại"});
-    }
-    console.log('File received:', req.file); // Log the file object
-    console.log('Body received:', req.body); // Log the form data
-    if (!req.file) {
-            return res.status(400).json({ message: "No image file uploaded" });
-        }
+  const existingProduct = await productModel.findOne({ name: req.body.name });
+  if (existingProduct) {
+    return res.json({ message: "Sản phẩm đã tồn tại" });
+  }
+  console.log("File received:", req.file); // Log the file object
+  console.log("Body received:", req.body); // Log the form data
+  if (!req.file) {
+    return res.status(400).json({ message: "No image file uploaded" });
+  }
 
-    // const limit = pLimit(2);
+  // const limit = pLimit(2);
 
-    // const imagesUpload = req.file.images.map((image) => {
-    //     return limit(async () => {
-    //         const result = await cloudinary.uploader.upload(image);
-    //         return result;
-    //     })
-    // })
+  // const imagesUpload = req.file.images.map((image) => {
+  //     return limit(async () => {
+  //         const result = await cloudinary.uploader.upload(image);
+  //         return result;
+  //     })
+  // })
 
-    // const uploadStatus = await Promise.all(imagesUpload)
-    // const imgURL = uploadStatus.map((image) => {
-    //     return image.secure_url
-    // })
+  // const uploadStatus = await Promise.all(imagesUpload)
+  // const imgURL = uploadStatus.map((image) => {
+  //     return image.secure_url
+  // })
 
-    // if (!uploadStatus) {
-    //     return res.status(500).json({status: false});
-    // }
+  // if (!uploadStatus) {
+  //     return res.status(500).json({status: false});
+  // }
 
-    //const image_file_names = req.files ? req.files.map(file => file.filename) : [];
-    //const image_filenames = req.files.map(file => file.filename);
-    const image_file_name = `${req.file.filename}`;
+  //const image_file_names = req.files ? req.files.map(file => file.filename) : [];
+  //const image_filenames = req.files.map(file => file.filename);
+  const image_file_name = `${req.file.filename}`;
 
-    console.log(image_file_name);
-    const product = new productModel({  
+  console.log(image_file_name);
+  const product = new productModel({
+    name: req.body.name,
+    // images: imgURL,
+    images: [image_file_name],
+    description: req.body.description,
+    price: req.body.price,
+    quantityStock: req.body.quantityStock,
+    category: req.body.category,
+  });
+  try {
+    await product.save();
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
+};
+
+const updateProducts = async (req, res) => {
+  const limit = pLimit(2);
+
+  const imagesUpload = req.body.images.map((image) => {
+    return limit(async () => {
+      const result = await cloudinary.uploader.upload(image);
+      return result;
+    });
+  });
+
+  const uploadStatus = await Promise.all(imagesUpload);
+  const imgURL = uploadStatus.map((image) => {
+    return image.secure_url;
+  });
+
+  if (!uploadStatus) {
+    return res.status(500).json({ status: false });
+  }
+
+  try {
+    const product = await productModel.findByIdAndUpdate(
+      req.params.id,
+      {
         name: req.body.name,
-        // images: imgURL,
-        images:[image_file_name],
+        images: imgURL,
         description: req.body.description,
         price: req.body.price,
         quantityStock: req.body.quantityStock,
-        category: req.body.category
-    })
-    try {
-        await product.save();
-        res.json({success: true});
-    } catch (error) {
-        console.log(error);
-        res.json({success: false});
-    }
-}
-
-const updateProducts = async (req, res) => {
-    const limit = pLimit(2);
-
-    const imagesUpload = req.body.images.map((image) => {
-        return limit(async () => {
-            const result = await cloudinary.uploader.upload(image);
-            return result;
-        })
-    })
-
-    const uploadStatus = await Promise.all(imagesUpload)
-    const imgURL = uploadStatus.map((image) => {
-        return image.secure_url
-    })
-
-    if (!uploadStatus) {
-        return res.status(500).json({status: false});
-    }
-
-    try {
-        const product = await productModel.findByIdAndUpdate(
-            req.params.id,
-            {
-                name: req.body.name,
-                images: imgURL,
-                description: req.body.description,
-                price: req.body.price,
-                quantityStock: req.body.quantityStock,
-                category: req.body.category
-            },
-            {new: true}
-        );
-        if (!product) return res.status(404).json({message: "Product not found"});
-        res.json({success: true, message: "Product updated"});
-    } catch (error) {
-        console.log(error);
-    }
-}
+        category: req.body.category,
+      },
+      { new: true }
+    );
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    res.json({ success: true, message: "Product updated" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const deleteProducts = async (req, res) => {
-    const productImage = await productModel.findById(req.params.id);
-    fs.unlink(`./uploads/${productImage.images[0]}`, (err) => {     
-        if (err) {
-            console.log(err);
-        }
-    });
-    try {
-        const product = await productModel.findByIdAndDelete(req.params.id);
-        if (!product) 
-            return res.status(404).json({message: "Product not found"});
-        res.json({success: true, message: "Product deleted"});
-    } catch (error) {
-        console.log(error);
+  const productImage = await productModel.findById(req.params.id);
+  fs.unlink(`./uploads/${productImage.images[0]}`, (err) => {
+    if (err) {
+      console.log(err);
     }
-}
+  });
+  try {
+    const product = await productModel.findByIdAndDelete(req.params.id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+    res.json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = { getProducts, getProductID, addProducts, deleteProducts, updateProducts }
+const getProductBasedOnCategory = async (req, res) => {
+  try {
+    const { categoryId } = req.params;
+    // console.log(categoryId);
+    const products = await productModel
+      .find({ category: categoryId })
+      .populate("category");
+
+    if (!products || products.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No products found for this category" });
+    }
+
+    // Return the found products
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching products", error });
+  }
+};
+
+module.exports = {
+  getProducts,
+  getProductID,
+  addProducts,
+  deleteProducts,
+  updateProducts,
+  getProductBasedOnCategory,
+};
