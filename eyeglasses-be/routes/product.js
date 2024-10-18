bebd625d22dea5cc752a49763c386f8d981addae
LeMinhMan2809 <lemann.cth@gmail.com>
2024-10-18 10:51:34 +0700

    Product routes

--- eyeglasses-be/routes/product.js
+++ eyeglasses-be/routes/product.js
@@ -5,25 +5,32 @@ const path = require("path");
 // const image = require("../uploads")
 
 const Product = require("../models/product");
-const { getProducts, addProducts, getProductID, updateProducts, deleteProducts } = require("../controllers/productController");
+const {
+  getProducts,
+  addProducts,
+  getProductID,
+  updateProducts,
+  deleteProducts,
+  getProductBasedOnCategory,
+} = require("../controllers/productController");
 
 const productRouter = express.Router();
 
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}_${file.originalname}`);
-        //return cb(null, Date.now() + path.extname(file.originalname));
-    }
-})
-const upload = multer({ storage: storage }); 
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}_${file.originalname}`);
+    //return cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
 
-
-productRouter.get("/", getProducts)
+productRouter.get("/", getProducts);
 productRouter.get("/:id", getProductID);
-productRouter.post("/add",upload.single("images"), addProducts)
+productRouter.get("/category/:categoryId", getProductBasedOnCategory);
+// productRouter.get("/category", getProductBasedOnCategory);
+productRouter.post("/add", upload.single("images"), addProducts);
 productRouter.put("/:id", updateProducts);
 productRouter.delete("/:id", deleteProducts);
-
 
 module.exports = productRouter;
