d6a7533baf1cd086444f6f5681783da89289e8b3
LeMinhMan2809 <lemann.cth@gmail.com>
2024-10-27 23:43:26 +0700

    Modify user route and add route for change password

--- eyeglasses-be/routes/user.js
+++ eyeglasses-be/routes/user.js
@@ -3,25 +3,50 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const path = require("path");
 // const image = require("../uploads")
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const { getUsers, getUserID, addUser, userLogin} = require("../controllers/userController");
+const {
+  getUsers,
+  getUserID,
+  addUser,
+  userLogin,
+  getOneUser,
+  updateUser,
+  forgotPassword,
+  resetPassword,
+} = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 const storage = multer.diskStorage({
-    destination: "uploads",
-    filename: (req, file, cb) => {
-        return cb(null, `${Date.now()}_${file.originalname}`);
-    }
-})
-const upload = multer({ storage: storage }); 
+  destination: "uploads/user",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
 
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(403).json({ message: "No token provided" });
 
-userRouter.get("/", getUsers)
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(500).json({ message: "Failed to authenticate token" });
+
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+userRouter.get("/", getUsers);
+userRouter.get("/profile", verifyToken, getOneUser);
 userRouter.get("/:id", getUserID);
-userRouter.post("/register",addUser);
-userRouter.post("/login",userLogin);
-
-
+userRouter.post("/register", upload.single("image"), addUser);
+userRouter.post("/login", userLogin);
+userRouter.put("/:id", upload.single("image"), updateUser);
+userRouter.post("/pw/forgot-password", forgotPassword);
+userRouter.post("/pw/reset-password/:id/:token", resetPassword);
 
 module.exports = userRouter;
