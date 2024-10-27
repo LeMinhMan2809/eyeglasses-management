fd45fb3857c9dc0330474405940d360c3946ff2e
LeMinhMan2809 <lemann.cth@gmail.com>
2024-10-27 23:58:20 +0700

    Brand controller

---
+++ eyeglasses-be/controllers/brandController.js
@@ -0,0 +1,76 @@
+const brandModel = require("../models/brand");
+
+const getBrands = async (req, res) => {
+  try {
+    const brands = await brandModel.find();
+    res.json(brands);
+  } catch (error) {
+    console.log(error);
+    res.json({ sucess: false });
+  }
+};
+
+const getCategoriesID = async (req, res) => {
+  try {
+    const category = await categoryModel.findById(req.params.id);
+    if (!category)
+      return res.status(404).json({ message: "Category not found" });
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
+};
+
+const addBrand = async (req, res) => {
+  const existingBrand = await brandModel.findOne({ name: req.body.name });
+  if (existingBrand) {
+    return res.json({ message: "Brand already exists" });
+  }
+
+  const brand = new brandModel({
+    name: req.body.name,
+    description: req.body.description,
+  });
+  try {
+    await brand.save();
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
+};
+
+const updateBrand = async (req, res) => {
+  try {
+    const brand = await brandModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        description: req.body.description,
+      },
+      { new: true }
+    );
+    if (!brand) return res.status(404).json({ message: "Brand not found" });
+    res.json({ success: true, message: "Brand updated" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteBrand = async (req, res) => {
+  try {
+    const brand = await brandModel.findByIdAndDelete(req.params.id);
+    if (!brand) return res.status(404).json({ message: "Brand not found" });
+    res.json({ success: true, message: "Brand deleted" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getBrands,
+  addBrand,
+  updateBrand,
+  deleteBrand,
+};
