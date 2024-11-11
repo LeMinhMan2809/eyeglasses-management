4fff318d117f428f94220c044f133157e348c383
LeMinhMan2809 <lemann.cth@gmail.com>
2024-11-11 23:39:04 +0700

    Define new API endpoint

--- eyeglasses-be/index.js
+++ eyeglasses-be/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const multer = require("multer");
+const http = require("http");
+const socketIO = require("socket.io");
 
 //Routes//
 const productRoute = require("./routes/product");
@@ -17,14 +19,50 @@ const addressRoute = require("./routes/address");
 
 const connectDB = require("./config/db");
 const orderRouter = require("./routes/order");
-const statusRouter = require("./routes/status");
+// const statusRouter = require("./routes/status");
 
 const reviewRouter = require("./routes/review");
+const brandRoute = require("./routes/brand");
 // const vnpayRouter = require("./routes/orderOnline");
+
+const orderDetailRouter = require("./routes/orderDetail");
+const wishRouter = require("./routes/wish");
+const supplierRouter = require("./routes/supplier");
+const inventoryRouter = require("./routes/inventory");
+const inventoryDetailRouter = require("./routes/inventoryDetail");
+const blogRouter = require("./routes/blog");
 
 require("dotenv").config();
 
 const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: {
+    origin: ["http://localhost:5172", "http://localhost:5173"], // React app port
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true, // Optional: Allow cookies to be sent across origins
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
+
+  // Listen for messages from the client
+  socket.on("send_message", (data) => {
+    console.log("Received message:", data);
+
+    // Broadcast message to all connected clients (admin)
+    io.emit("receive_message", data);
+  });
+
+  // User disconnected
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
@@ -34,6 +72,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/product", productRoute);
+app.use("/api/brand", brandRoute);
 app.use("/images", express.static("uploads"));
 app.use("/images", express.static("uploads/user"));
 app.use("/api/category", categoryRoute);
@@ -45,14 +84,23 @@ app.use("/api/city", cityRoute);
 app.use("/api/district", districtRoute);
 app.use("/api/ward", wardRoute);
 
-app.use("/api/order", orderRouter);
-app.use("/api/status", statusRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/orderDetail", orderDetailRouter);
+// app.use("/api/status", statusRouter);
 
 app.use("/api/review", reviewRouter);
+app.use("/api/wishlist", wishRouter);
+
+app.use("/api/supplier", supplierRouter);
+
+app.use("/api/blog", blogRouter);
+
+app.use("/api/inventory", inventoryRouter);
+app.use("/api/inventoryDetail", inventoryDetailRouter);
 
 //DB Connection
 connectDB();
 
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server is running http://localhost:${process.env.PORT}`);
 });
