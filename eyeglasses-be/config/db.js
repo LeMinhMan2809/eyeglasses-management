79adb2d15664e4dce29fea7638291b4a9303f38d
LeMinhMan2809 <lemann.cth@gmail.com>
2024-11-18 22:55:01 +0700

    Modify connect DB

--- eyeglasses-be/config/db.js
+++ eyeglasses-be/config/db.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-       .then(() => {
-        console.log("DB Connected")
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("DB Connected");
     })
     .catch((err) => {
-        console.log(err)});
-}
+      console.log(err);
+    });
+};
 
-module.exports = connectDB
+module.exports = connectDB;
